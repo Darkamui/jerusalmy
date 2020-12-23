0fd9526992c4d683ed690f904e808ef014c6f7e8
Daniel Jerusalmy <dannyerushalmi@gmail.com>
2020-12-22 22:36:00 -0500

    svg landing anim

--- src/App.js
+++ src/App.js
@@ -1,47 +1,46 @@
-import React,{useState} from 'react';
-import {Switch,Route,useLocation} from 'react-router-dom';
+import React, { useState } from "react";
+import { Switch, Route, useLocation } from "react-router-dom";
 //Import Components
-import Nav from './components/Nav';
+import Nav from "./components/Nav";
 //Import Styles
-import './styles/app.scss';
+import "./styles/app.scss";
 //Import Utils
-import Landing from './components/Landing';
-import Ouvrages from './pages/Ouvrages';
-import LibraryBook from './pages/LibraryBook';
-import About from './pages/About';
-import {AnimatePresence} from 'framer-motion';
-
+import Landing from "./components/Landing";
+import Ouvrages from "./pages/Ouvrages";
+import LibraryBook from "./pages/LibraryBook";
+import About from "./pages/About";
+import Text from "./components/Text";
+import { AnimatePresence } from "framer-motion";
 
 export default function App() {
-    const location = useLocation();
-    //State
-    const [libraryStatus, setLibraryStatus] = useState(false);
+	const location = useLocation();
+	//State
+	const [libraryStatus, setLibraryStatus] = useState(false);
 
-    return (
-        <div>
-            <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus}/>
-                <AnimatePresence exitBeforeEnter>
-                    <Switch location={location} key={location.pathname}>
-                        <Route path="/" exact>
-                            <Landing/>
-                        </Route>
+	return (
+		<div>
+			<Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+			<AnimatePresence exitBeforeEnter>
+				<Switch location={location} key={location.pathname}>
+					<Route path='/' exact>
+						<Landing />
+						<Text />
+					</Route>
 
-                        <Route path="/ouvrages" exact>
-                            <Ouvrages/>
-                        </Route>
-                        <Route path="/auteur" exact>
-                            <About/>
-                        </Route>
-                        <Route path="/:id">
-                            <LibraryBook/>
-                        </Route>
-                        
-                    </Switch>
-                </AnimatePresence>
-            
-            
-            {/* <Book currentBook={currentBook}/>
+					<Route path='/ouvrages' exact>
+						<Ouvrages />
+					</Route>
+					<Route path='/auteur' exact>
+						<About />
+					</Route>
+					<Route path='/:id'>
+						<LibraryBook />
+					</Route>
+				</Switch>
+			</AnimatePresence>
+
+			{/* <Book currentBook={currentBook}/>
             <Library books={books} libraryStatus={libraryStatus}  currentBook={currentBook} setCurrentBook={setCurrentBook}/> */}
-        </div>
-    )
+		</div>
+	);
 }
